5c18572ae48f69a563f777ff508eba24ce973fdb
Pavittar Singh <singh.pavittar95@icloud.com>
2020-05-28 18:22:19 +0530

    db state set in while starting the app

--- db/index.js
+++ db/index.js
@@ -1,8 +1,12 @@
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
-
+const data = require("./db.json");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
+
+if (!Object.keys(db.getState()).length) {
+  db.setState(data);
+}
 
 const createPetModel = require("./pet");
 const createUserModel = require("./user");
--- db/pet.js
+++ db/pet.js
@@ -2,8 +2,6 @@ const nanoid = require("nanoid");
 
 const createPetModel = (db) => ({
   findMany: (filter = {}) => {
-    console.log(db.get("pet").find({ id: "5ebb8bd465ab8516cb75dbe8" }).value());
-
     return db
       .get("pet")
       .filter(filter)
